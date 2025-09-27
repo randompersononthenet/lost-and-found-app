69a13639ab166da98671ce6bc8a47c1f8cb41ec1
Aedrian Sagap <sagapaedrian.com>
2025-09-28 00:05:40 +0800

    feat: configured the admin client to be able to deployed on Vervel

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 5174
-	}
+	},
+	build: {
+		outDir: 'dist',
+		assetsDir: 'assets',
+		sourcemap: false,
+		minify: 'terser'
+	},
+	base: '/admin/'
 }) 
