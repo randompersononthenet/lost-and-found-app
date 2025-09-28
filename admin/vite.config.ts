de926da24dfdf0d9949403fb93b2a3a57ea8e2bd
Aedrian Sagap <sagapaedrian.com>
2025-09-28 18:17:35 +0800

    feat: fixed config for local development for admin web client

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
 	plugins: [react()],
 	server: {
 		port: 5174
@@ -13,5 +13,5 @@ export default defineConfig({
 		sourcemap: false,
 		minify: 'esbuild'
 	},
-	base: '/admin/'
-}) 
+	base: command === 'build' ? '/admin/' : '/'
+})) 
